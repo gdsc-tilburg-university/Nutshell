2e2d70f11dc6e6663b77ab4beb14b9a861228b4b
Lumalizer <7863320+Lumalizer@users.noreply.github.com>
2022-11-04 19:04:41 +0100

    record and transcribe (simple)

--- main.py
+++ main.py
@@ -1,5 +1,32 @@
+import sounddevice as sd
+from scipy.io.wavfile import write
 import whisper
+from os import getcwd
 
-model = whisper.load_model("base")
-result = model.transcribe("audio.mp3")
-print(result["text"])
+
+def audio_transcription(audiofile: str) -> str:
+    model = whisper.load_model("base")
+    result = model.transcribe(audiofile)
+    return result["text"]
+
+
+def summarize(text: str) -> str:
+    return text
+
+# transcription = audio_transcription("audio.mp3")
+# summary = summarize(transcription)
+
+
+def record(duration: int = 10):
+    fs = 44100
+    myrecording = sd.rec(duration * fs, samplerate=fs,
+                         channels=2, dtype='float64')
+    print("Recording Audio")
+    sd.wait()
+    print("Audio recording complete , Play Audio")
+    target = f'{getcwd()}\\audio\\latest.wav'
+    write(target, rate=44100, data=myrecording)
+    return target
+
+
+print(audio_transcription(record()))
